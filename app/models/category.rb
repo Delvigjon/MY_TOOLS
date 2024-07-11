b4edb2dc0a2c952b540d39ab4a124f229d65d1b5
Frederico Qasem Rodrigues De Sousa <fredesousa@icloud.com>
2024-07-11 15:18:24 +0200

    add images

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ApplicationRecord
   has_many :tools_categories, dependent: :destroy
   has_many :tools, through: :tools_categories
+  has_one_attached :image
 
   validates :name, presence: true
 end
