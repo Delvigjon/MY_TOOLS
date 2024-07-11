1eedf4588625aea769308ab7809ac6f65b7d23b1
jonathan delvig <delvigjon@hotmail.fr>
2024-07-11 18:04:58 +0200

    booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,4 +5,11 @@ class Booking < ApplicationRecord
   has_one_attached :image
 
   validates :start_date, :end_date, presence: true
+  before_save :total_price
+
+  private
+
+  def total_price
+    (end_date - start_date).to_i * 
+  end
 end
